c3f0022df5995e94c08b5e23b5f9eef9a0752c1e
wxyzcctv <2846629226@qq.com>
2019-03-24 15:38:37 +0800

    将高度换以视高为基准

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     let duration = 10
     let id
     $('.actions').on('click','button',function(e){
-        let $button = $(e.currentTarget)//获得当前点击的按钮是哪一个
+        let $button = $(e.currentTarget)//获得当前点击的按钮是那一个
         let speed = $button.attr('data-speed')
         $button.addClass('active')
         .siblings('.active').removeClass('active')
@@ -31,7 +31,7 @@
         let n = 0
         id = setTimeout(function run() {
             n += 1
-            // container.innerHTML = Prism.highlight( code.substring(0,n),Prism.languages.css)
+            // container.innerHTML = code.substring(0,n)
             container.innerHTML = Prism.highlight( code.substring(0,n),Prism.languages.css)
             styleTage.innerHTML = code.substring(0, n)
             container.scrollTop = container.scrollHeight
@@ -56,121 +56,122 @@
         border-radius: 50%;
       }
       #spot1 {
-        width: 120px;
-        height: 200px;
-        border-radius: 120px / 200px;
-        top: 10px;
-        left: 20px;
+        width: 15vh;
+        height: 25vh;
+        border-radius: 15vh / 25vh;
+        top: 3vh;
+        left: 5vh;
         transform: rotate(-15deg);
       }
-    #spot2 {
-        width: 60px;
-        height: 100px;
-        border-radius: 60px / 100px;
-        top: 220px;
-        left: 20px;
+      #spot2 {
+        width: 7.5vh;
+        height: 12.5vh;
+        border-radius: 7.5vh / 12.5vh;
+        top: 38vh;
+        left: 15vh;
         transform: rotate(-15deg);
       }
        #spot3 {
-        width: 120px;
-        height: 180px;
-        border-radius: 120px / 180px;
-        top: 500px;
-        left: 230px;
+        width: 15vh;
+        height: 22.5vh;
+        border-radius: 15vh / 22.5vh;
+        top: 75vh;
+        left: 30vh;
         transform: rotate(5deg);
       }
       #spot4 {
-        width: 60px;
-        height: 100px;
-        border-radius: 60px / 100px;
-        top: 480px;
-        left: 20px;
+        width: 7.5vh;
+        height: 12.5vh;
+        border-radius: 7.5vh / 12.5vh;
+        top: 60vh;
+        left: 2.5vh;
         transform: rotate(0deg);
       }
       #spot5 {
-        width: 40px;
-        height: 80px;
-        border-radius: 40px / 80px;
-        top: 50px;
-        right: 70px;
+        width: 5vh;
+        height: 10vh;
+        border-radius: 5vh / 10vh;
+        top: 6vh;
+        right: 9vh;
         transform: rotate(-15deg);
       }
       #spot6 {
-        width: 150px;
-        height: 180px;
-        border-radius: 150px / 180px;
-        top: 400px;
-        right: 20px;
+        width: 20vh;
+        height: 25vh;
+        border-radius: 20vh / 25vh;
+        top: 50vh;
+        right: 2.5vh;
         transform: rotate(10deg);
       }
       #spot7 {
-        width: 60px;
-        height: 80px;
-        border-radius: 60px / 80px;
-        top: 580px;
-        right: 180px;
+        width: 7.5vh;
+        height: 10vh;
+        border-radius: 7.5vh / 10vh;
+        top: 75vh;
+        right: 25vh;
         transform: rotate(25deg);
       }
       #face {
         position: relative;
-        width: 600px;
+        width: 75vh;
         margin: 0 auto;
         transform: scale(1, 1);
       }
       #eyes {
-        width: 540px;
+        width: 67.5vh;
         margin: 0 auto;
         position: relative;
-        top: 100px;
+        top: 12.5vh;
+        border:1px solid red;
       }
       .lash {
-        border: 7px solid black;
+        border: 0.875vh solid black;
         background-color: black;
         width: 0px;
-        height: 100px;
+        height: 12.5vh;
       }
       #leftlash1 {
         position: absolute;
-        top: -30px;
-        left: 80px;
+        top: -4.25vh;
+        left: 10vh;
         transform: rotate(-35deg);
       }
       #leftlash2 {
         position: absolute;
-        top: -40px;
-        left: 125px;
+        top: -5vh;
+        left: 15.625vh;
         transform: rotate(0deg);
       }
       #leftlash3 {
         position: absolute;
-        top: -30px;
-        left: 170px;
+        top: -4.25vh;
+        left: 21.25vh;
         transform: rotate(35deg);
       }
       #rightlash1 {
         position: absolute;
-        top: -30px;
-        right: 170px;
+        top: -4.25vh;
+        right: 21.25vh;
         transform: rotate(-35deg);
       }
       #rightlash2 {
         position: absolute;
-        top: -40px;
-        right: 125px;
+        top: -5vh;
+        right: 15.625vh;
         transform: rotate(0deg);
       }
       #rightlash3 {
         position: absolute;
-        top: -30px;
-        right: 80px;
+        top: -4.25vh;
+        right: 10vh;
         transform: rotate(35deg);
       }
     .eye {
         overflow: hidden;
-        border: 10px solid black;
+        border: 1.25vh solid black;
         background-color: white;
-        width: 250px;
-        height: 250px;
+        width: 31.25vh;
+        height: 31.25vh;
         border-radius: 100%;
       }
       #lefteye {
@@ -183,77 +184,77 @@
       }
       .iris {
         transition: all 0.1s ease-in-out;
-        border: 10px solid black;
+        border: 1.25vh solid black;
         background-color: #48CAFE;
-        width: 100px;
-        height: 100px;
+        width: 12.5vh;
+        height: 12.5vh;
         border-radius: 100%;
       }
       #leftiris {
         position: absolute;
-        right: 30px;
-        top: 60px;
+        right: 4.25vh;
+        top: 8.5vh;
       }
       #leftiris:hover {
-        right: 0px;
+        right: 0vh;
       }
     #rightiris{
       position:absolute;
-      left:30px;
-      top:60px;
+      left:4.25vh;
+      top:8.5vh;
     }
     #rightiris:hover{
-      left:0px;
+      left:0vh;
     }
       .pupil {
-        border: 10px solid black;
+        border: 1.25vh solid black;
         background-color: black;
-        width: 50px;
-        height: 50px;
+        width: 6.25vh;
+        height: 6.25vh;
         position: absolute;
-        left: 15px;
-        top: 15px;
-        border-radius: 50%;
+        left: 2vh;
+        top: 2vh;
+        border-radius: 100%;
       }
       #nose {
-        border-width: 10px 10px 0 10px;
+        border-width: 1.25vh 1.25vh 0 1.25vh;
         border-style: solid;
         border-color: rgb(0, 0, 0);
         position: absolute;
-        top: 240px;
-        left: calc(50% - 50px);
-        width: 100px;
-        height: 150px;
+        top: 30vh;
+        left: calc(50% - 6.25vh);
+        width: 12.5vh;
+        height: 18.75vh;
         border-radius: 50% 50% 60% 60%;
         background-color:yellow;
       }
       .cheek {
-        border-width: 10px 10px 0 10px;
+        border-width: 1.25vh 1.25vh 0 1.25vh;
         border-style: solid;
         border-color: rgb(231, 45, 22);
-        width: 100px;
-        height: 90px;
+        width: 12.5vh;
+        height: 11.25vh;
         border-radius: 50%;
         background-image: 
-        radial-gradient(6px at 75px 25px, rgb(231, 45, 22) 0%, 
+        radial-gradient(0.75vh at 9.375vh 3.125vh, rgb(231, 45, 22) 0%, 
         rgb(231, 45, 22) 100%, transparent),
-        radial-gradient(6px at 50px 50px, rgb(231, 45, 22) 0%, 
+        radial-gradient(0.75vh at 6.25vh 6.25vh, rgb(231, 45, 22) 0%, 
         rgb(231, 45, 22) 100%, transparent),
-        radial-gradient(6px at 25px 25px, rgb(231, 45, 22) 0%, 
+        radial-gradient(0.75vh at 3.125vh 3.125vh, rgb(231, 45, 22) 0%, 
         rgb(231, 45, 22) 100%, transparent);
       }
       #leftcheek {
         position: absolute;
         background-color: yellow;
-        top: 300px;
-        left: 20px;
+        top: 37.5vh;
+        left: 2.5vh;
         transform: rotate(-25deg);
       }
       #rightcheek {
         position: absolute;
         background-color: yellow;
-        top: 300px;
-        right: 20px;
+        top: 37.5vh;
+        right: 2.5vh;
         transform: rotate(25deg);
       }
       #mouth {
@@ -262,39 +263,39 @@
         overflow: hidden;
         background-color: rgb(77, 0, 10);
         position: absolute;
-        top: 380px;
-        left: calc(50% - 225px);
-        width: 450px;
-        height: 200px;
-        border:10px solid rgb(0, 0, 0);
+        top: 47.5vh;
+        left: calc(50% - 28.125vh);
+        width: 56.25vh;
+        height: 25vh;
+        border:1.25vh solid rgb(0, 0, 0);
         border-radius: 0 0 30% 30%;
         background-image:
-        radial-gradient(55px at 245px 200px, rgb(248, 158, 167) 0%, 
+        radial-gradient(6.875vh at 30.625vh 25vh, rgb(248, 158, 167) 0%, 
         rgb(248, 158, 167) 100%,  transparent),
-        radial-gradient(60px at 195px 200px, rgb(248, 158, 167) 0%, 
+        radial-gradient(7.5vh at 24.375vh 25vh, rgb(248, 158, 167) 0%, 
         rgb(248, 158, 167) 90%, #000 91%, #000 100%, transparent),
-        radial-gradient(60px at 245px 200px, rgb(248, 158, 167) 0%, 
+        radial-gradient(7.5vh at 30.625vh 25vh, rgb(248, 158, 167) 0%, 
         rgb(248, 158, 167) 90%, #000 91%, #000 100%, transparent)
           }
       #mouth:hover {
         height: 0px;
       }
       .tooth {
-        border:10px solid rgb(0,0,0);
+        border:1.25vh solid rgb(0,0,0);
         background-color: rgb(255, 255, 255);
-        width: 70px;
-        height: 80px;
+        width: 8.75vh;
+        height: 10vh;
       }
       #lefttooth {
         position: absolute;
-        top: -20px;
-        left: 125px;
+        top: -2.5vh;
+        left: 15.625vh;
         transform: rotate(2deg);
       }
       #righttooth {
         position: absolute;
-        top: -20px;
-        right: 125px;
+        top: -2.5vh;
+        right: 15.625vh;
         transform: rotate(-2deg);
       }
     `
